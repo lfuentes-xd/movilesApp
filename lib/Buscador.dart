9c01abca5d4fd475adafb28e1925f9320b52c351
Carlos Mauricio Macías González <carlitos.mauricio08@hotmail.com>
2023-12-05 09:34:42 -0600

    buscador

--- lib/Buscador.dart
+++ lib/Buscador.dart
@@ -1,96 +1,101 @@
 import 'package:flutter/material.dart';
 
-class Buscador extends StatelessWidget {
+class Car {
+  final String brand;
+  final String model;
+  final String imagePath; // Cambiando el nombre a imagePath
+
+  Car({
+    required this.brand,
+    required this.model,
+    required this.imagePath, // Cambiando el nombre a imagePath
+  });
+}
+
+
+
+class Buscador extends StatefulWidget {
+  @override
+  _BuscadorState createState() => _BuscadorState();
+}
+
+class _BuscadorState extends State<Buscador> {
+  List<Car> cars = [
+    Car(
+      brand: 'Nissan',
+      model: 'X-Trail',
+      imagePath: 'lib/img/Xtrail.jpeg',
+    ),
+    Car(
+      brand: 'Nissan',
+      model: 'Maxima',
+      imagePath: 'lib/img/maxima.jpg',
+    ),
+    Car(
+      brand: 'Nissan',
+      model: 'Altima',
+      imagePath: 'lib/img/altima.jpeg',
+    ),
+    // Agrega más objetos Car según sea necesario
+  ];
+
+  String filter = '';
+
   @override
   Widget build(BuildContext context) {
+    List<Car> filteredCars = cars.where((car) {
+      final brandLower = car.brand.toLowerCase();
+      final modelLower = car.model.toLowerCase();
+      final filterLower = filter.toLowerCase();
+      return brandLower.contains(filterLower) || modelLower.contains(filterLower);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bienvenido a CarStore"), 
-        actions: [
+        title: Text("CarStore"),
+        backgroundColor: Colors.orange[600],
+      ),
+      body: Column(
+        children: [
           Padding(
-            padding: EdgeInsets.only(left: 9.0, right: 19.0), // Ajustar el padding aquí
-            child: IconButton(
-              onPressed: () {
-                showSearch(
-                  context: context,
-                  delegate: CustomSearchDelegate(),
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  filter = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Buscar',
+                hintText: 'Escribe aquí para buscar',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredCars.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    leading: Image.network(
+                      filteredCars[index].imagePath,
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(filteredCars[index].brand),
+                    subtitle: Text(filteredCars[index].model),
+                  ),
                 );
               },
-              icon: const Icon(Icons.search),
             ),
           ),
         ],
-        backgroundColor: Colors.orange[600],
-      ),
-      body: const Center(
-        child: Text('Hello World'),
       ),
     );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchTerms = [
-    'toyota',
-    'mustang',
-  ];
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    ); // ListView.builder
   }
 }
